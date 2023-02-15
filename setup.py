159bafbb9c4b5460d2a89270f20a42cd68e89767
gioSacchi <44093534+gioSacchi@users.noreply.github.com>
2023-02-15 16:48:12 +0100

    init cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My master's thesis projet on computation of counterfactual explanations using bayesian optimization.',
+    author='Giorgio Sacchi',
+    license='MIT',
+)
