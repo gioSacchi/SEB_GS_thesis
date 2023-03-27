c58f61531b6277d51a5d26d4d6376b065b6ff9ba
pingu <giorgiosacchi@me.com>
2023-03-27 18:06:42 +0200

    small stuff

--- BO_test.py
+++ BO_test.py
@@ -1,4 +1,5 @@
 from BO import BayesianOptimization
+import matplotlib.pyplot as plt
 
 import numpy as np
 
@@ -10,12 +11,14 @@ def g(X, noise=0):
     return -np.sin(5*X) - X**2 + 0.7*X + noise * np.random.randn(*X.shape)
 
 def bo_test():
-    bounds = np.array([[-1.0, 2.0], [-2.0, 1.0]])
-    # bounds = np.array([[-1.0, 2.0]])
-    bo = BayesianOptimization(g, 2, bounds=bounds, n_iter=100, n_init=25, noise_std=0)
-    # bo = BayesianOptimization(f, 1, bounds=bounds, n_iter=10, n_init=5, noise_std=0)
+    # bounds = np.array([[-1.0, 2.0], [-2.0, 1.0]])
+    bounds = np.array([[-1.0, 2.0]])
+    # bo = BayesianOptimization(g, 2, bounds=bounds, n_iter=42, n_init=25, noise_std=0)
+    bo = BayesianOptimization(f, 1, bounds=bounds, n_iter=10, n_init=2, noise_std=0)
     bo.run_BO()
     bo.make_plots()
+
+    plt.show()
 
 if __name__ == "__main__":
     bo_test()
