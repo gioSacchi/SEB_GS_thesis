50384cf2424b48e2f660c9b0b457da9276436546
pingu <giorgiosacchi@me.com>
2023-04-18 12:52:33 +0200

    work on experiments

--- income.py
+++ income.py
@@ -251,6 +251,73 @@ def comparison(model, df_train, opt_func = None, objective_function=None, acq_fu
     alt_quasi.run()
     alt_quasi.make_plots()
 
+def experiment_1_1D(df_train, model):
+    # init data
+    input_data = df_train.drop("Income", axis=1)
+    output_data = df_train["Income"]
+    std = input_data.std().to_numpy().reshape(-1)
+    min1 = input_data.iloc[:, 0].min()
+    max1 = input_data.iloc[:, 0].max()
+    margin1 = (max1 - min1) * 0.1
+    bounds = np.array([[min1 - margin1, max1 + margin1]])
+
+    grid = 100
+
+    # generated points between min and max of data
+    x = np.linspace(input_data.min(), input_data.max(), grid)
+    current_points = x.reshape(-1, 1)
+    # generate grid of desired output values between min and max of output data
+    desired = np.linspace(output_data.min(), output_data.max(), grid)
+    base_lambda = 10
+
+    n_starting_points = 2
+    n_max_iterations = 15
+    n_stop = 4
+
+    evals = {"BO": [], "Random": [], "Quasi": [], "Sep_BO": []}
+    error = {"BO": [], "Random": [], "Quasi": [], "Sep_BO": []}
+    for point in current_points:
+        for y_prim in desired:
+            # define objective function
+            lam = base_lambda/y_prim**2
+            objective_func = lambda x_prim, func_val: f(point, x_prim, y_prim, func_val, lam, std=std)
+            opt_func = lambda x_prim: f(point, x_prim, y_prim, model.predict(x_prim), lam, std=std)
+            dist_func = lambda x_prim, current_point: np.linalg.norm(np.divide(x_prim-current_point, std), axis=1)
+            acq_func = CF_acquisition(dist_func, y_prim, x, lam).get_CF_EI()
+            # run experiment
+            # Normal BO
+            bo_normal = BayesianOptimization(f = opt_func, acquisition="EI", dim = 1, bounds = bounds, 
+                                      n_iter=n_max_iterations, n_init=n_starting_points, n_stop_iter=n_stop, 
+                                      noise_std=0, normalize_Y=True)
+            bo_normal.run_BO()
+            evals["BO"].append(bo_normal.number_of_evaluations_f)
+            # Separated BO
+            bo_separated = BayesianOptimization(f = model.predict, obj_func=objective_func, acquisition=acq_func, dim = 1, 
+                                                bounds = bounds, n_iter=n_max_iterations, n_init=n_starting_points,  
+                                                n_stop_iter=n_stop, noise_std=0, normalize_Y=True)
+            bo_separated.run_BO()
+            evals["Sep_BO"].append(bo_separated.number_of_evaluations_f)
+            # Random
+            alt_random = ComparisonOptimizers(opt_func, 1, bounds=bounds, method = 'random',
+                                                n_iter=n_max_iterations, n_init=n_starting_points, n_stop_iter=n_stop,
+                                                noise_std=0)
+            alt_random.run()
+            evals["Random"].append(alt_random.n_evals)
+            # Quasi
+            alt_quasi = ComparisonOptimizers(opt_func, 1, bounds=bounds, method='l-bfgs-b',
+                                                n_iter=n_max_iterations, n_init=n_starting_points, n_stop_iter=n_stop,
+                                                noise_std=0)
+            alt_quasi.run()
+            evals["Quasi"].append(alt_quasi.n_evals)
+
+
+
+
+
+
+
+
+
 def main():
     """Main function"""
     data_path = "src\data\Income1.csv"
@@ -290,25 +357,25 @@ def main():
     # # init f for BO
     # func = lambda x: model.predict(x)
 
-    # objective function for BO
-    x = np.array([[14]])
-    y_prim = 50
-    lam = 5/y_prim**2
-    func = lambda x_prim: f(x, x_prim, y_prim, model.predict(x_prim), lam, std=std)
-    print("x, punkt i fråga", x)
-    print("y_prim, önskad output", y_prim)
-    print("f(x), nuvarande output", func(x))
-    BO_on_regressor(func, df_train)
+    # # objective function for BO
+    # x = np.array([[14]])
+    # y_prim = 50
+    # lam = 5/y_prim**2
+    # func = lambda x_prim: f(x, x_prim, y_prim, model.predict(x_prim), lam, std=std)
+    # print("x, punkt i fråga", x)
+    # print("y_prim, önskad output", y_prim)
+    # print("f(x), nuvarande output", func(x))
+    # BO_on_regressor(func, df_train)
 
-    # For testing own acquisition function
-    # define functions
-    x = np.array([[14]])
-    y_prim = 50
-    lam = 10/y_prim**2
-    objective_func = lambda x_prim, func_val: f(x, x_prim, y_prim, func_val, lam, std=std)
-    dist_func = lambda x_prim, current_point: np.linalg.norm(np.divide(x_prim-current_point, std), axis=1)
-    acq_func = CF_acquisition(dist_func, y_prim, x, lam).get_CF_EI()
-    bo_test_own(model.predict, objective_func, acq_func, df_train, y_prim=y_prim)
+    # # For testing own acquisition function
+    # # define functions
+    # x = np.array([[14]])
+    # y_prim = 50
+    # lam = 10/y_prim**2
+    # objective_func = lambda x_prim, func_val: f(x, x_prim, y_prim, func_val, lam, std=std)
+    # dist_func = lambda x_prim, current_point: np.linalg.norm(np.divide(x_prim-current_point, std), axis=1)
+    # acq_func = CF_acquisition(dist_func, y_prim, x, lam).get_CF_EI()
+    # bo_test_own(model.predict, objective_func, acq_func, df_train, y_prim=y_prim)
 
     # # compare with other optimizers
     # x = np.array([[14]])
